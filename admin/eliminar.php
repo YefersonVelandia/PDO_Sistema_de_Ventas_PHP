9f8eb30f9d17ea3c16d226566abb6188fb53f487
yeferson <yefersonvelandia4@gmail.com>
2021-12-13 16:49:20 -0500

    Implemetada accion de admin -> eliminar producto

---
+++ admin/eliminar.php
@@ -0,0 +1,14 @@
+<?php
+	if(!isset($_GET["id"])) exit();
+		$id = $_GET["id"];
+
+	include_once '../conexion.php';
+	$sentencia = $con->prepare("DELETE FROM productos WHERE id = ?");
+	$resultado = $sentencia->execute([$id]);
+
+	if($resultado === TRUE){
+		header("Location: ./inventario.php");
+		exit;
+	}
+	else echo "Algo salió mal";
+?>
